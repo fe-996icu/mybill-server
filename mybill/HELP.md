2c4c58696103ea828857fbfee3b97b2047a62cac
zhangjianzhong <zhangjianzhong>
2025-02-15 13:52:00 +0800

    新增一个springboot+mybatis模块

---
+++ mybill/HELP.md
@@ -0,0 +1,8 @@
+# mybill
+
+个人记账系统
+
+> SpringBoot with MyBatisPlus
+>
+> Java JDK Version 17
+>
---
+++ mybill/src/main/java/com/icu/mybill/MybillApplication.java
@@ -0,0 +1,13 @@
+package com.icu.mybill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybillApplication.class, args);
+    }
+
+}
