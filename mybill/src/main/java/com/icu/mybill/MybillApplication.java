067046d2952cd318dc2736d007cce1e5972aa968
zhangjianzhong <zhangjianzhong>
2025-02-15 14:02:51 +0800

    启动类添加mapper扫描注解

--- mybill/src/main/java/com/icu/mybill/MybillApplication.java
+++ mybill/src/main/java/com/icu/mybill/MybillApplication.java
@@ -1,9 +1,11 @@
 package com.icu.mybill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.icu.mybill.mapper") // 扫描mapper接口
 public class MybillApplication {
 
     public static void main(String[] args) {
