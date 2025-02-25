2a0b6e78fb1bb29475a39c05713b407539910f75
zhangjianzhong <zhangjianzhong>
2025-02-25 21:28:11 +0800

    添加全局异常处理函数：sql语法错误和方法参数校验异常

--- mybill/src/main/java/com/icu/mybill/exception/GlobalExceptionHandler.java
+++ mybill/src/main/java/com/icu/mybill/exception/GlobalExceptionHandler.java
@@ -1,19 +1,36 @@
 package com.icu.mybill.exception;
 
 import com.icu.mybill.common.Result;
+import com.icu.mybill.dto.accountbook.UpdateAccountBookSortDTO;
 import com.icu.mybill.enums.ResultCode;
 import com.icu.mybill.exception.user.LoginException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.jdbc.Null;
+import org.springframework.context.MessageSourceResolvable;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
+import org.springframework.validation.method.ParameterValidationResult;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Slf4j
 // @RestControllerAdvice 表示全局异常处理器，拦截所有 @Controller 抛出的异常
@@ -90,13 +107,14 @@ public class GlobalExceptionHandler {
 
     /**
      * 请求体格式错误异常
-     * 参数校验异常，没有传递参数，spring无法解析转换请求参数为DTO
+     * 参数校验异常，spring无法解析转换请求参数为DTO
+     * 1. 没有传递参数
+     * 2. 请求体格式错误，比如接收是list但传递的map
      * @param e
      * @return
      */
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public Result<?> handleNotReadableBodyException(HttpMessageNotReadableException e) {
-        String errorMessage = e.getLocalizedMessage();
         log.warn("[HttpMessageNotReadableException]", e);
         return Result.build(null, ResultCode.PARAMETER_FAIL);
     }
@@ -111,6 +129,39 @@ public class GlobalExceptionHandler {
         log.warn("[MissingServletRequestParameterException]", e);
         return Result.build(null, ResultCode.PARAMETER_FAIL.getCode(), String.format("缺少参数 %s %s", e.getParameterType(), e.getParameterName()));
     }
+
+    /**
+     * 请求参数校验异常 RequestBody检查
+     * 1. 请求参数是list，并且list中的DTO字段检查会走这里
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public Result<?> handleHandlerMethodValidationException(HandlerMethodValidationException e) {
+        List<String> errors = new ArrayList<>();
+
+        for (int i = 0; i < e.getParameterValidationResults().size(); i++) {
+            ParameterValidationResult validationResult = e.getParameterValidationResults().get(i);
+            List<? extends MessageSourceResolvable> allErrors = e.getAllErrors();
+
+            FieldError fieldError = (FieldError) allErrors.get(i);
+
+            // 异常数据在提交list参数中的索引
+            Integer index = validationResult.getContainerIndex();
+            // 校验不通过的字段名
+            String field = fieldError.getField();
+            // 前端传递过来校验不通过的字段值
+            Object rejectedValue = fieldError.getRejectedValue();
+            // 校验不通过的错误信息
+            String defaultMessage = fieldError.getDefaultMessage();
+
+            errors.add(String.format("第%d条的%s", index + 1, defaultMessage));
+        }
+
+        log.warn("[HandlerMethodValidationException] {}", e.getParameterValidationResults());
+        return Result.build(null, ResultCode.PARAMETER_FAIL.getCode(), String.join(";", errors));
+    }
+
     /**
      * 登录异常
      * @param e
@@ -135,4 +186,14 @@ public class GlobalExceptionHandler {
         throw e;
     }
 
+    /**
+     * SQL语法错误异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(BadSqlGrammarException.class)
+    public Result<?> handleBadSqlGrammarException(BadSqlGrammarException e) {
+        log.warn("[BadSqlGrammarException]", e);
+        return Result.build(null, ResultCode.SQL_SYNTAX_ERROR);
+    }
 }
