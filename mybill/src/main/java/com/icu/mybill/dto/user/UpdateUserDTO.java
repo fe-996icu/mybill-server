9f5b8324028c630747d5d24143c35a28077ae382
zhangjianzhong <zhangjianzhong>
2025-03-02 22:31:44 +0800

    修改用户数据DTO添加校验注解

--- mybill/src/main/java/com/icu/mybill/dto/user/UpdateUserDTO.java
+++ mybill/src/main/java/com/icu/mybill/dto/user/UpdateUserDTO.java
@@ -1,11 +1,13 @@
 package com.icu.mybill.dto.user;
 
+import com.icu.mybill.annotation.AtLeastOneNotNull;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@AtLeastOneNotNull
 @Schema(description = "更新用户信息DTO")
 public class UpdateUserDTO {
     @NotBlank(message = "昵称不能为空")
